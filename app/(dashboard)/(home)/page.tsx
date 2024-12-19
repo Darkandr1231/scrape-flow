0ad5bdc35a3abaeec0ec1b35df80d83b01593356
Darkandr1231 <markandrey05@gmail.com>
2024-12-19 19:44:20 +0600

    thirteenth

--- app/(dashboard)/(home)/page.tsx
+++ app/(dashboard)/(home)/page.tsx
@@ -1,18 +1,40 @@
 import { GetPeriods } from '@/actions/analytics/getPeriods';
 import React, { Suspense } from 'react'
+import PeriodSelector from './_components/PeriodSelector';
+import { Period } from '@/types/analytics';
+import { waitFor } from '@/lib/helper/waitFor';
+import { Skeleton } from '@/components/ui/skeleton';
 
-function HomePage() {
+function HomePage({
+  searchParams,
+}:{
+  searchParams: {month?: string, year?: string};
+}) {
+  const currentDate = new Date();
+  const {month, year} = searchParams;
+  const period: Period = {
+    month: month ? parseInt(month) : currentDate.getMonth(),
+    year: year ? parseInt(year) : currentDate.getFullYear(),
+  };
   return (
-    <div>
-      <Suspense>
-        <PeriodSelectorWrapper />
-      </Suspense>
+    <div className="flex flex-1 flex-col h-full">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold">Home</h1>
+        <Suspense fallback={<Skeleton className="w-[180px] h-[40px]" />}>
+          <PeriodSelectorWrapper selectedPeriod={period} />
+        </Suspense>
+      </div>
+      Stats
     </div>
   )
 }
 
-async function PeriodSelectorWrapper() {
+async function PeriodSelectorWrapper({
+  selectedPeriod,
+}: {
+  selectedPeriod : Period;
+}) {
   const periods = await GetPeriods();
-  return <pre>{JSON.stringify(periods, null, 4)}</pre>
+  return <PeriodSelector selectedPeriod={selectedPeriod} periods={periods} />;
 }
 export default HomePage
